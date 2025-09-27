75d5debb8c2a52f17e5ff76d7cd6e03319b7ebda
Christian Yana <cyanahu@unsa.edu.pe>
2025-09-27 14:31:06 -0500

    se agrega el segundo ejercicio con main , metodos adicionales , solo pivot

---
+++ Ejercicio2/ejercicio2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ejercicio2 {
+    public static void main(String[] args) {
+        float arr [] = {5.12f,7.27f,9.14f,2.17f,6.12f,4.14f,4.13f,17.46f,17.01f};
+        
+        quicksort(arr,0 ,arr.length-1);
+        for (float num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void quicksort(float arr[], int inferior,int techo){
+        if(inferior<techo){
+            int pivot = partir(arr,inferior,techo);
+
+        }
+    }
+    public static int partir(float arr[], int inferior, int techo) {
+        int medio = (inferior + techo)/2; //indice del medio
+        float pivote = arr[medio];
+        
+
+        
+    }
+}
