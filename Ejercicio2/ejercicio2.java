71f87a534d1db5a7397396995150f526c85447c0
Christian Yana <cyanahu@unsa.edu.pe>
2025-09-27 16:57:01 -0500

    agreamos los metodos para el pivot aleatorio(copiar lo mismo de la forma anterior)

--- Ejercicio2/ejercicio2.java
+++ Ejercicio2/ejercicio2.java
@@ -8,9 +8,14 @@ public class ejercicio2 {
         for (float num : arr) {
             System.out.print(num + " ");
         }
+        
+        float arr2 [] = {5.12f,7.27f,9.14f,2.17f,6.12f,4.14f,4.13f,17.46f,17.01f};
+        System.out.println("\nPivot - aleatorio");
+        quicksort2(arr2,0 ,arr.length-1);
 
-        System.out.println("Pivot - aleatorio");
-        quicksort2(arr,0 ,arr.length-1);
+        for (float num : arr2) {
+            System.out.print(num + " ");
+        }
     }
 
     public static void quicksort(float arr[], int inferior,int techo){
@@ -22,10 +27,9 @@ public class ejercicio2 {
         }
     }
     public static int partir(float arr[], int inferior, int techo) {
-        int medio = (inferior + techo)/2; //indice del medio
-        float pivote = arr[medio];
+        int indiceMedio = (inferior + techo)/2; //indice del medio
+        float pivote = arr[indiceMedio];
         
-
         while (true) {
             while (arr[inferior] < pivote) {
                 inferior++;
@@ -49,9 +53,37 @@ public class ejercicio2 {
     }
 
     public static void quicksort2(float arr[], int inferior,int techo){
+        if(inferior<techo){
+            int pivot = partir2(arr,inferior,techo);
 
+            quicksort2(arr, inferior, pivot);     
+            quicksort2(arr, pivot + 1, techo);
+        }
     }
     public static int partir2(float arr[], int inferior, int techo){
-        return 1 ;
+        int indiceAleatorio = (int) (Math.random()*(techo - inferior + 1)) + inferior; //indice aletorio
+        float pivote = arr[indiceAleatorio];
+        
+
+        while (true) {
+            while (arr[inferior] < pivote) {
+                inferior++;
+            }
+            while (arr[techo] > pivote) {
+                techo--;
+            }
+
+            if (inferior >= techo) {
+                return techo;
+            }
+
+            // Intercambiar
+            float temp = arr[inferior];
+            arr[inferior] = arr[techo];
+            arr[techo] = temp;
+
+            inferior++;
+            techo--;
+        }
     }
 }
