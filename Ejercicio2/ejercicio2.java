dd15f7396b1ee28c1de92c94b710c19b17862d11
Christian Yana <cyanahu@unsa.edu.pe>
2025-09-27 14:43:02 -0500

    agregamos metodos para el segundo pivot que es aleatorio

--- Ejercicio2/ejercicio2.java
+++ Ejercicio2/ejercicio2.java
@@ -3,10 +3,14 @@ public class ejercicio2 {
     public static void main(String[] args) {
         float arr [] = {5.12f,7.27f,9.14f,2.17f,6.12f,4.14f,4.13f,17.46f,17.01f};
         
+        System.out.println("Pivot - Medio");
         quicksort(arr,0 ,arr.length-1);
         for (float num : arr) {
             System.out.print(num + " ");
         }
+
+        System.out.println("Pivot - aleatorio");
+        quicksort2(arr,0 ,arr.length-1);
     }
 
     public static void quicksort(float arr[], int inferior,int techo){
@@ -42,5 +46,12 @@ public class ejercicio2 {
             inferior++;
             techo--;
         }
+    }
+
+    public static void quicksort2(float arr[], int inferior,int techo){
+
+    }
+    public static int partir2(float arr[], int inferior, int techo){
+        return 1 ;
     }
 }
