2e7f262144522f069a6abfe9418014bacf911a88
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-09-26 23:50:11 -0500

    implementando el metodo merge para complementar al metodo mergeSort

--- Ejercicio1/ejercicio1.java
+++ Ejercicio1/ejercicio1.java
@@ -13,12 +13,50 @@ public class ejercicio1{
         estudiates[7] = new Estudiante("Lucia", 19.5f, "A008");
 
     }
-    public static void mergeSort(Estudiante[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
+    public static void mergeSort(Estudiante[] arr, int izquierda, int derecha) {
+        if (izquierda < derecha) {
+            int medio = (izquierda + derecha) / 2;
+            mergeSort(arr, izquierda, medio);
+            mergeSort(arr, medio + 1, derecha);
+            merge(arr, izquierda, medio, derecha);
+        }
+    }
+    public static void merge(Estudiante[] arr, int izquierda, int medio, int derecha) {
+        int numeroElementosIzquierda = medio - izquierda + 1; //se incluye el elemento del medio
+        int numeroElementosDerecha = derecha - medio;
+
+        Estudiante[] L = new Estudiante[numeroElementosIzquierda]; //array temporal
+        Estudiante[] R = new Estudiante[numeroElementosDerecha]; //array temporal
+
+        for (int i = 0; i < numeroElementosIzquierda; i++) //llenando los arrays temporales
+            L[i] = arr[izquierda + i];
+        for (int j = 0; j < numeroElementosDerecha; j++) //llenando los arrays temporales
+            R[j] = arr[medio + 1 + j];
+
+        int i = 0, j = 0;
+        int k = izquierda;
+        while (i < numeroElementosIzquierda && j < numeroElementosDerecha) {
+            if (L[i].getPromedio() <= R[j].getPromedio()) {
+                arr[k] = L[i];
+                i++;
+            } 
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < numeroElementosIzquierda) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < numeroElementosDerecha) {
+            arr[k] = R[j];
+            j++;
+            k++;
         }
     }
 
