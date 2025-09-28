088b55dd3c750399a5d8e4b847264b39398e3bd6
Christian Yana <cyanahu@unsa.edu.pe>
2025-09-27 22:26:37 -0500

    argregamos el ejercicio3 , reutilizamos el quicksort del ejercicio2

---
+++ Ejercicio3/ejercicio3.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        int tamaño = 100000;
+        int[] arreglo = generarArregloAleatorio(tamaño);
+        int elementoBuscado = arreglo[new Random().nextInt(tamaño)];
+
+        int[] arregloOrdenado = new int[arreglo.length];
+
+
+        for (int i = 0; i < arreglo.length; i++) {
+            arregloOrdenado[i] = arreglo[i];
+        }
+
+        quicksort(arregloOrdenado, 0, arregloOrdenado.length - 1);
+
+    }
+
+    public static int[] generarArregloAleatorio(int tamaño) {
+        Random aleatorio = new Random();
+        int[] arreglo = new int[tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            arreglo[i] = aleatorio.nextInt(tamaño * 10);
+        }
+        return arreglo;
+    }
+
+    public static void quicksort(int arr[], int inferior,int techo){
+        if(inferior<techo){
+            int pivot = partir(arr,inferior,techo);
+
+            quicksort(arr, inferior, pivot);     
+            quicksort(arr, pivot + 1, techo);
+        }
+    }
+    public static int partir(int arr[], int inferior, int techo) {
+        int indiceAleatorio = (int) (Math.random()*(techo - inferior + 1)) + inferior; //indice aletorio
+        int pivote = arr[indiceAleatorio];
+        
+        while (true) {
+            while (arr[inferior] < pivote) {
+                inferior++;
+            }
+            while (arr[techo] > pivote) {
+                techo--;
+            }
+
+            if (inferior >= techo) {
+                return techo;
+            }
+
+            // Intercambiar
+            int temp = arr[inferior];
+            arr[inferior] = arr[techo];
+            arr[techo] = temp;
+
+            inferior++;
+            techo--;
+        }
+    }
+}
